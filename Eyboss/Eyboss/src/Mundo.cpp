671b0dab675f429a59301c8af59a30f98c240425
sandrarebollo10@gmail.com <sandrarebollo10@gmail.com>
2020-04-30 18:02:43 +0200

    Prueba 1

--- Eyboss/Eyboss/src/Mundo.cpp
+++ Eyboss/Eyboss/src/Mundo.cpp
@@ -60,7 +60,7 @@ void Mundo::Tecla(unsigned char key)
 			protagonista.SetVel(protagonista.GetVel().x, protagonista.GetSalto());
 			protagonista.SetAcel(protagonista.GetAcel().x, -9.8f);
 			protagonista.SetOn(false);
-			//ETSIDI::play("sonidos/jump.wav");
+			ETSIDI::play("sonidos/jump.wav");
 		}
 		break;		
 	case 'q': //Cierra la pantalla
@@ -87,7 +87,7 @@ void Mundo::TeclaEspecial(unsigned char key) {
 		else if (vel.x < 0)
 			d->SetVel(-7.0f, 0.0f);		
 		disparos.Agregar(d);
-		//ETSIDI::play("sonidos/shoot.wav");
+		ETSIDI::play("sonidos/shoot.wav");
 		break;
 	}
 }
