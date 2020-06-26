45f5e09325ee9a38835ca83d3015c180ba0a3971
mati dgg <mati.dgg@hotmail.com>
2020-06-26 13:10:51 +0200

    (master) * Finalizado de Espada (Parte 2/2) * Finalizada la inclusión de Espada con todas sus características * Se han añadido: seguir al personaje, acabado con el problema de instakill al ocultarse después de dañar a un enemigo * Se ha añadido la función seguir y el atributo tiempo0 a Objeto

--- Eyboss/Eyboss/src/O_Espada.cpp
+++ Eyboss/Eyboss/src/O_Espada.cpp
@@ -1,22 +1,35 @@
+
 #include "O_Espada.h"
 #include "Interaccion.h"
+#include "Global.h"
 
 void O_Espada::Inicializa() {
 	sprite = new ETSIDI::Sprite(textura, 0.0f, 0.0f, ancho, altura);
 }
 
 void O_Espada::Dibuja() {
-	glColor3f(0.0f, 1.0f, 1.0f);
-	glPushMatrix();
-	glTranslatef(posicion.x, posicion.y, 0);
-	if (orientacion)
-		sprite->flip(false, false);
-	else
-		sprite->flip(true, false);;
-	sprite->draw();
-	//glTranslatef(-posicion.x, -posicion.y, 0);
-	glPopMatrix();
+
+	if (m_tiempo) {
+		tiempo0 = Global::tiempo;
+		m_tiempo = false;
+	}
+
+	if ((Global::tiempo - tiempo0) / 40 < 0.5 && Global::tiempo / 40 > 0.5) {
+		glColor3f(0.0f, 1.0f, 1.0f);
+		glPushMatrix();
+		glTranslatef(posicion.x, posicion.y, 0);
+		if (orientacion)
+			sprite->flip(false, false);
+		else
+			sprite->flip(true, false);
+		sprite->draw();
+		//glTranslatef(-posicion.x, -posicion.y, 0);
+		glPopMatrix();
+	}
+	if ((Global::tiempo - tiempo0) / 40 > 0.5 || m_golpe)
+		tiempo0 = 0;
 }
+
 
 void O_Espada::SetPos(float ex, float ey) {
 	posicion.x = ex;
@@ -27,7 +40,8 @@ Vector2D O_Espada::GetPos() {
 	return posicion;
 }
 
-void O_Espada::Ataca(Personaje& p) {
+void O_Espada::Seguir(Personaje& p) {
+
 	orientacion = p.GetOrientacion();
 	float dist;
 	if (orientacion)
@@ -36,6 +50,12 @@ void O_Espada::Ataca(Personaje& p) {
 		dist = p.GetPos().x - (p.GetAncho() / 2 + ancho / 2);
 
 	SetPos(dist, p.GetPos().y);
+}
+
+void O_Espada::Ataca(Personaje& p) {
+	m_tiempo = true;
+	m_golpe = false;
+
 	ETSIDI::play("bin/sonidos/Espada1.wav");
 }
 
@@ -44,6 +64,7 @@ void O_Espada::Colision(ListaEnemigos& l) {
 		if (Interaccion::Colision(*this, *l[i])) {
 			if (l[i]->GetVida() > 0) {
 				l[i]->SetVida(-3);
+				m_golpe = true;
 			}
 			else {
 				l.Eliminar(l[i]);
@@ -52,3 +73,4 @@ void O_Espada::Colision(ListaEnemigos& l) {
 		}
 	}
 }
+
